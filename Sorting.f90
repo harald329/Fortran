07671cfd4ce464d14fcf3b2a3fe571bd37cb6c8d
Harald Toft <113995366+harald329@users.noreply.github.com>
2024-03-04 18:51:36 +0100

    Update Sorting.f90
    changed how time is measured

--- Sorting.f90
+++ Sorting.f90
@@ -2,53 +2,42 @@
 
 PROGRAM sorting
         ! Declares variables
-        REAL, DIMENSION(10) :: array
-        INTEGER :: arraySize, C, R, M, I, NBR
-        INTEGER PRE (8), POST (8), RES (8)
-        CHARACTER (LEN = 10) BIG_BEN (3)
-
-        NBR = 100000
-        arraySize = 10
-
-        ! creates array
+        INTEGER, PARAMETER :: arraySize = 1000
+        INTEGER :: I, NBR = 1000
+        REAL :: array(arraySize)
+        REAL first, second, resu 
+       
         I = 0
-        ! Mesaures time for before sorting
-        call measureTime(PRE,BIG_BEN)
+        call cpu_time(first) 
                 do while(I < NBR)
                         CALL resetArray(array)
                         CALL bubbleSort(array)
                         I = I + 1
                 end do
-        call measureTime(POST,BIG_BEN)
-        call postcalc(POST,PRE,array,NBR)
+        call cpu_time(second)
+        resu = second - first
+        write(*,*) 'Time for bubble sort',NBR,'times :',resu,'for arrays with a size of', arraySize,'elements'
 
         I = 0
-        call measureTime(PRE,BIG_BEN)
+        call cpu_time(first)
                 do while(I < NBR)
                          CALL resetArray(array)
                          CALL insertSort(array)
                          I = I + 1
                 end do                         
-        call measureTime(POST,BIG_BEN)
-        call postcalc(POST,PRE,array,NBR)
-
+        call cpu_time(second)
+        resu = second - first
+        write(*,*) 'Time for insert sort',NBR,'times :',resu,'for arrays with a size of',arraysize,'elements'
 END PROGRAM
 
-SUBROUTINE measureTime(T,BIG_BEN)
-        CHARACTER (LEN = 10) BIG_BEN (3)
-        INTEGER T(8) 
-        CALL DATE_AND_TIME (BIG_BEN (1), BIG_BEN (2), BIG_BEN (3), T)
-END SUBROUTINE
-
 SUBROUTINE resetArray(array)
-        REAL, DIMENSION(10) :: array
-        INTEGER :: I, array_size
+        REAL, DIMENSION(n) :: array
+        INTEGER :: I 
         I = 1
-        array_size = SIZE(array) 
         ! Sets all the values in array to random real numbers between 0 and 1
         CALL RANDOM_NUMBER(HARVEST = array)
         ! Functions like a while statement in java
-        do while(I<array_size+1)
+        do while(I<SIZE(array)+1)
                 array(I) = array(I)*100
                 ! CEILING: Rounds of to nearest smaller integer
                 array(I) = CEILING(array(I))
@@ -57,12 +46,11 @@ SUBROUTINE resetArray(array)
 END SUBROUTINE
 
 SUBROUTINE bubbleSort(array)
-        REAL, DIMENSION(10) :: array
-        INTEGER :: I, swap, array_size, J
-        array_size = SIZE(array) 
+        REAL, DIMENSION(n) :: array
+        INTEGER :: I, swap, J
         I = 1
-        do while(I < array_size)
-                J = array_size
+        do while(I < SIZE(array))
+                J = SIZE(array) 
                 do while(J>I)
                         if(array(J)<array(J-1)) then
                                 swap = array(J)
@@ -75,29 +63,18 @@ SUBROUTINE bubbleSort(array)
         end do
 END SUBROUTINE
 
-SUBROUTINE postcalc(POST, PRE, array, NBR)
-        INTEGER PRE(8), POST(8), RES(8)
-        INTEGER :: NBR
-        REAL, DIMENSION(10) :: array
-        RES = POST - PRE
-        write(*,*) 'Sorted using insert sort ', NBR, ' times' 
-        write(*,*) 'Time to sort ', RES 
-        write(*,*) array
-END SUBROUTINE
-
 SUBROUTINE insertSort(array)
-        REAL, DIMENSION(10) :: array
-        INTEGER :: i, j, array_size, swap
-        i = 2
-        array_size = SIZE(array)
-        do while(i < array_size+1) 
-                j = i
-                do while(array(j)<array(j-1)) 
-                        swap = array(j)
-                        array(j) = array(j-1)
-                        array(j-1) = swap
-                        j = j - 1
+        REAL, DIMENSION(n) :: array
+        INTEGER :: I, swap, J
+        I = 2
+        do while(I < SIZE(array)+1) 
+                J = I
+                do while(array(J)<array(J-1)) 
+                        swap = array(J)
+                        array(J) = array(J-1)
+                        array(J-1) = swap
+                        J = J - 1
                 end do
-                i = i + 1
+                I = I + 1
         end do
 END SUBROUTINE
